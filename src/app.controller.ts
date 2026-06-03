620e248bc58e57b4089435d329a88a00e38e6574
yassakhairy28 <yassa7000@gmail.com>
2026-06-03 16:31:28 +0300

    feat: implement error handling and validation middleware

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import db_connection from "./DB/db_connection.js";
+import {
+  globalErrorHandler,
+  notFoundHanlder,
+} from "./middlewares/error.handler.middleware.js";
 
 const appController = async (app: express.Application) => {
+  await db_connection();
+  app.get("/", (req, res) => res.send("Welcome to ledger-task Backend API!"));
 
-    await db_connection()
-  app.get("/", (req, res) => res.send("Hello World!"));
+  app.all("/*dummy", notFoundHanlder);
+  app.use(globalErrorHandler);
 };
 
 export default appController;
